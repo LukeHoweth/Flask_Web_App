ce58032a5ddf20abe935fa5ffffc9c2f549fa497
LukeHoweth <lukehoweth30@gmail.com>
2024-02-19 13:04:04 +0000

    Design and create database

--- website/__init__.py
+++ website/__init__.py
@@ -1,13 +1,29 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'thematic project'
+    app.config["SECRET_KEY"] = "thematic project"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{path.join(path.abspath('website'), DB_NAME)}"# Telling flask where my db is stored
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
 
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
-
+    from .models import User, Note
+    create_database(app)
+    
     return app
+
+def create_database(app):
+    db_path = path.join(path.abspath('website'), DB_NAME)
+    if not path.exists(db_path):            # If database doesn't exist,
+        with app.app_context():             # create it.
+            db.create_all()  
+            print("Created Database!")
--- website/auth.py
+++ website/auth.py
@@ -1,22 +1,22 @@
 from flask import Blueprint, render_template, request, flash
 
-auth = Blueprint('auth', __name__)
+auth = Blueprint("auth", __name__)
 
-@auth.route('/login', methods=['GET', 'POST'])
+@auth.route("/login", methods=["GET", "POST"])
 def login():
     return render_template("login.html")
 
-@auth.route('/logout')
+@auth.route("/logout")
 def logout():
     return "<p>Logout</p>"
 
-@auth.route('/sign-up', methods=['GET', 'POST'])
+@auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        firstName = request.form.get('firstName')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
+    if request.method == "POST":
+        email = request.form.get("email")
+        firstName = request.form.get("firstName")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
 
         #===== Validating input information before adding to database =====
         if len(email) < 4:
