6ebfc48195bd0d2ec21579ba2fb9f14ed1f23889
Luke Howeth <53517302+LukeHoweth@users.noreply.github.com>
2024-03-09 22:37:34 +0000

    Update index.js

--- website/static/index.js
+++ website/static/index.js
@@ -1,9 +1,8 @@
 function deleteNote(noteID) {
-    console.log("cliiiiiick");
     fetch("/delete-note", {
         method: "POST",
         body: JSON.stringify({ noteID: noteID }),
     }).then((_res) => {
-        window.location.href = "/";
-    });
+        window.location.href = "/"
+    })
 }
